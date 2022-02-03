3648c65eeaef9ca0772be59f922d0464d3f97c01
puck621 <92011971+puck621@users.noreply.github.com>
2022-02-03 15:45:12 -0700

    Update word.py

--- game/word.py
+++ game/word.py
@@ -5,8 +5,6 @@ class Word:
         my_file = open("game/word_list.txt", "r")
         dataString = my_file.read()
         self.wordlist = dataString.split("\n")
-        print(self.wordlist)
-        print(len(self.wordlist))
 
     def get_Word(self):
         return self.wordlist[random.randint(0, len(self.wordlist)-1)]
