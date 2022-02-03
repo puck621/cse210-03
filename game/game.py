866715964c6fb9c53abe0dbeb298f7557df06375
puck621 <92011971+puck621@users.noreply.github.com>
2022-02-03 02:13:45 -0700

    Update game.py

--- game/game.py
+++ game/game.py
@@ -1,24 +1,24 @@
-import random
-import sys
+from game.word import Word
+from game.player import Player
+from game.display import Display
 
 class Game:
     def __init__(self):
         self.still_playing = ""
         self.player = Player()
         self.word = Word()
-        self.secret_word = words.get_word()
+        self.secret_word = word.get_word()
         self.display = Display(secrect_word, player)
-        
-       
+               
     def main(self):
         still_playing = True
         while player.lives > 0 and keepGoing == True:
-            guess = input("Guess a letter [a-z] or type "quit" to quit: ")
+            guess = input("Guess a letter [a-z] or type q to quit: ")
             while len(guess) > 1:
                 print("Please only input one letter")
                 guess = input("Guess a letter [a-z]: ")
                 print("You guessed: ", guess)
-            if guess == "quit":
+            if guess == "q":
                 still_playing = False
                 print("The secret word is ", secrect_word)
                 break
