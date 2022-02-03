d8228fb4e8e2b9fbb03cd9e979fe3cb44e1f38a7
cub525 <49500036+cub525@users.noreply.github.com>
2022-02-03 14:43:58 -0700

    __str__ method for player

--- game/game.py
+++ game/game.py
@@ -1,18 +1,15 @@
 from game.word import Word
 from game.player import Player
-from game.display import Display
 
 class Game:
     def __init__(self):
         self.still_playing = ""
         self.player = Player()
         self.word = Word()
-        self.secret_word = word.get_word()
-        self.display = Display(secrect_word, player)
                
     def main(self):
         still_playing = True
-        while player.lives > 0 and keepGoing == True:
+        while self.player.lives > 0 and still_playing == True:
             guess = input("Guess a letter [a-z] or type q to quit: ")
             while len(guess) > 1:
                 print("Please only input one letter")
@@ -20,22 +17,14 @@ class Game:
                 print("You guessed: ", guess)
             if guess == "q":
                 still_playing = False
-                print("The secret word is ", secrect_word)
+                print("The secret word is ", self.word.word)
                 break
             for letter in theword:
                 if letter == guess:
-                    display.foundLetter(letter)
-                    display.display_word()
-                    display.diplay_person
+                    # Placeholder for correct guesses
+                    # Placeholder for printing the word
+                    print(self.player)
         
-
-
-
-
-
-
-
-    
 
 if __name__ == "__main__":
     game = Game()
--- game/player.py
+++ game/player.py
@@ -1,10 +1,33 @@
 class Player:
     def __init__(self):
         self.lives = 5
-        self.attempts = 0
-        self.correct_counter = 0
         self.guesses = []
 
     def wrong_attempt(self):
-        self.lives = self.lives -1
-        self.attempts = self.attempts + 1
+        self.lives -= 1
+
+    def __str__(self):
+        str_ = ""
+        if self.lives == 5:
+            str_ += "  ___ \n"
+
+        if self.lives == 4:
+            str_ += " /___\\\n"
+
+        if self.lives == 3:
+            str_ += " \   /\n"
+
+        if self.lives == 2:
+            str_ += "  \ / \n"
+            
+        if self.lives == 1:
+            str_ += "   O  \n"
+
+        if self.lives == 0:
+            str_ += "  X  \n"
+
+        str_ += " /|\ \n"
+        str_ += " / \ \n"
+        str_ += "^^^^^\n"
+    
+        return str_
