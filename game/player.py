02395f51dbab9981af62d89f9c96242993e42929
puck621 <92011971+puck621@users.noreply.github.com>
2022-02-03 01:49:18 -0700

    Create player.py

---
+++ game/player.py
@@ -0,0 +1,10 @@
+class Player:
+    def __init__(self):
+        self.lives = 5
+        self.attempts = 0
+        self.correct_counter = 0
+        self.guesses = []
+
+    def wrong_attempt(self):
+        self.lives = self.lives -1
+        self.attempts = self.attempts + 1
