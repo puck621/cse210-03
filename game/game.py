4f5e79aec6754ee7085aa5ed0b5ec06e2f98cb70
puck621 <92011971+puck621@users.noreply.github.com>
2022-02-03 15:32:52 -0700

    Update game.py

--- game/game.py
+++ game/game.py
@@ -1,30 +1,45 @@
 from game.word import Word
 from game.player import Player
+from game.display import Display
 
 class Game:
     def __init__(self):
         self.still_playing = ""
         self.player = Player()
         self.word = Word()
-               
+        self.secret_word = self.word.get_Word()
+        self.display = Display(self.secret_word, self.player)
+
     def main(self):
-        still_playing = True
-        while self.player.lives > 0 and still_playing == True:
-            guess = input("Guess a letter [a-z] or type q to quit: ")
-            while len(guess) > 1:
-                print("Please only input one letter")
-                guess = input("Guess a letter [a-z]: ")
-                print("You guessed: ", guess)
-            if guess == "q":
-                still_playing = False
-                print("The secret word is ", self.word.word)
+        #For testing remove for real play
+        print("The secret word is ", self.secret_word)
+
+        self.still_playing = True
+        while self.player.lives > 0 and self.still_playing == True:
+            guess = input("Guess a letter [A-Z] or type quit to quit: ")
+            guess = guess.upper()
+            if guess == "quit":
+                self.still_playing = False
+                print("The secret word is ", self.secret_word)
                 break
-            for letter in theword:
+            elif len(guess) != 1:
+                print("Please only input one letter")
+                continue
+
+            print("You guessed: ", guess)
+            for letter in self.secret_word:
+                found = False
                 if letter == guess:
-                    # Placeholder for correct guesses
-                    # Placeholder for printing the word
-                    print(self.player)
-        
+                    self.display.foundLetter(letter)
+                    found = True
+                    break
+
+            if not found:
+              self.player.wrong_attempt()
+
+            self.display.display_word()
+            self.display.display_person()
+
 
 if __name__ == "__main__":
     game = Game()
