adfe8e40d147df0c70fddb8ed56a074a8cb6d7ee
puck621 <92011971+puck621@users.noreply.github.com>
2022-02-03 15:32:55 -0700

    Update player.py

--- game/player.py
+++ game/player.py
@@ -5,29 +5,3 @@ class Player:
 
     def wrong_attempt(self):
         self.lives -= 1
-
-    def __str__(self):
-        str_ = ""
-        if self.lives == 5:
-            str_ += "  ___ \n"
-
-        if self.lives == 4:
-            str_ += " /___\\\n"
-
-        if self.lives == 3:
-            str_ += " \   /\n"
-
-        if self.lives == 2:
-            str_ += "  \ / \n"
-            
-        if self.lives == 1:
-            str_ += "   O  \n"
-
-        if self.lives == 0:
-            str_ += "  X  \n"
-
-        str_ += " /|\ \n"
-        str_ += " / \ \n"
-        str_ += "^^^^^\n"
-    
-        return str_
