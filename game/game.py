c623cf5066d113b8873a0d28a9e59ad0415e1b1b
cub525 <49500036+cub525@users.noreply.github.com>
2022-02-03 17:34:44 -0700

    in keyword can return boolean

--- game/game.py
+++ game/game.py
@@ -4,7 +4,7 @@ from game.display import Display
 
 class Game:
     def __init__(self):
-        self.still_playing = ""
+        self.still_playing: bool
         self.player = Player()
         self.word = Word()
         self.secret_word = self.word.get_Word()
@@ -15,23 +15,18 @@ class Game:
         while self.player.lives > 0 and self.still_playing == True:
             guess = input("Guess a letter [A-Z] or type quit to quit: ")
             guess = guess.upper()
-            if guess == "quit":
+            if guess == "QUIT":
                 self.still_playing = False
                 print("The secret word is ", self.secret_word)
-                break
             elif len(guess) != 1:
                 print("Please only input one letter")
                 continue
 
             print("You guessed: ", guess)
-            for letter in self.secret_word:
-                found = False
-                if letter == guess:
-                    self.display.foundLetter(letter)
-                    found = True
-                    break
-
-            if not found:
+            if guess in self.secret_word:
+                self.display.foundLetter(guess)
+                print("You found a letter!")
+            else:
               self.player.wrong_attempt()
 
             self.display.display_word()
