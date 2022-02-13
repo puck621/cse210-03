551b5eab6d96e3404781ac4cd5e20282dfa17202
puck621 <92011971+puck621@users.noreply.github.com>
2022-02-13 03:33:39 -0700

    Update word.py
    update comments

--- game/word.py
+++ game/word.py
@@ -3,8 +3,19 @@ import random
 class Word:
     """Chooses a random word to be guessed by the player from the list of words in the word list"""
     def __init__(self):
+     """Imports full word list from the scrabble template
+    
+        Attributes:
+        wordlist: all available words"""  
+        
         with open("./game/word_list.txt", "r") as my_file:
             self.wordlist = my_file.readlines()
 
     def get_word(self):
+        
+       """Returns random word from full list of scrabble words
+    
+        Attributes:
+        wordlist: all available words"""  
+        
         return random.choice(self.wordlist).lower()
