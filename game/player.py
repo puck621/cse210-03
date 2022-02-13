0e841b4eb12ddfec7d7c27057f9551dea26adb82
puck621 <92011971+puck621@users.noreply.github.com>
2022-02-13 03:32:08 -0700

    Update player.py
    Update comments

--- game/player.py
+++ game/player.py
@@ -5,14 +5,32 @@ class Player:
     
     """
     def __init__(self):
+        
+        """Defines all attributes
+    
+    Attributes:
+        lives:  number of lives a player has left
+        attempts: number of guesses the player has made
+        correct_counter: number of correct guesses the player has made
+        guesses: list of letters the player has guessed"""
+            
         self.lives = 5
         self.attempts = 0
         self.correct_counter = 0
         self.guesses = []
 
     def wrong_attempt(self):
+       """Removes a player life for each wrong guess
+    
+    Attributes:
+        lives:  number of lives a player has left"""
+        
         self.lives -= 1
         self.attempts += 1
     
     def get_lives(self):
+        """Returns the number of lives for other classes
+    
+        Attributes:
+        lives:  number of lives a player has left"""
         return self.lives
